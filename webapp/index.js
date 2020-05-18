87cfb13a52cb833738050268f64cd886685f7abf
Daryl Tan Chin Ting <sc18ctdt@leeds.ac.uk>
2020-05-19 02:42:19 +0800

    Implement debouncer to delay get request

--- webapp/index.js
+++ webapp/index.js
@@ -8,7 +8,24 @@ const fetchData = async (searchQuery) => {
 	console.log(response.data);
 };
 
+//DEBOUNCER
+const debounce = (func, delay = 1000) => {
+	let timerId;
+	return (...args) => {
+		//clears timeout of the previous function call unless no event for (delay)
+		if (timerId) {
+			clearTimeout(timerId);
+		}
+		timerId = setTimeout(() => {
+			func.apply(null, args);
+		}, delay);
+	};
+};
+
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-	fetchData(event.target.value);
-});
+input.addEventListener(
+	'input',
+	debounce((event) => {
+		fetchData(event.target.value);
+	})
+);
