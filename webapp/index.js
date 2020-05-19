0426d71c931813535bd10e3584cbbdd3228afa10
Daryl Tan Chin Ting <sc18ctdt@leeds.ac.uk>
2020-05-20 03:47:44 +0800

    Extract and add data-value into statistics HTML element for comparison

--- webapp/index.js
+++ webapp/index.js
@@ -24,22 +24,26 @@ const autocompleteConfig = {
 	}
 };
 
+//Left Column
 createAutocomplete({
 	...autocompleteConfig,
 	root: document.querySelector('#left-autocomplete'),
 	onOptionSelect(movie) {
-		onMovieSelect(movie, document.querySelector('#left-details'));
+		onMovieSelect(movie, document.querySelector('#left-details'), 'left');
 	}
 });
+//Right Column
 createAutocomplete({
 	...autocompleteConfig,
 	root: document.querySelector('#right-autocomplete'),
 	onOptionSelect(movie) {
-		onMovieSelect(movie, document.querySelector('#right-details'));
+		onMovieSelect(movie, document.querySelector('#right-details'), 'right');
 	}
 });
 
-const onMovieSelect = async (movie, detailsElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, detailsElement, column) => {
 	//Fetch movie details of selected movie
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
@@ -50,10 +54,37 @@ const onMovieSelect = async (movie, detailsElement) => {
 
 	//Render movie details
 	renderMovieDetails(response.data, detailsElement);
+
+	//Compare movies
+	if (column === 'left') {
+		leftMovie = response.data;
+	} else {
+		rightMovie = response.data;
+	}
+
+	if (leftMovie && rightMovie) {
+		compareMovies(leftMovie, rightMovie);
+	}
 };
 
-const renderMovieDetails = (movieDetails, parentElement) => {
-	parentElement.innerHTML = `
+const renderMovieDetails = (movieDetails, detailsElement) => {
+	//Retrieve and parse movie statistics to number to run comparison
+	const boxOffice =
+		movieDetails.BoxOffice === 'N/A' ? 0 : parseInt(movieDetails.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+	const metascore = movieDetails.Metascore === 'N/A' ? 0 : parseInt(movieDetails.Metascore);
+	const imdbRating = movieDetails.imdbRating === 'N/A' ? 0 : parseFloat(movieDetails.imdbRating);
+	const imdbVotes = movieDetails.imdbVotes === 'N/A' ? 0 : parseInt(movieDetails.imdbVotes.replace(/,/g, ''));
+	const awards = movieDetails.Awards.split(' ').reduce((accumulator, word) => {
+		const value = parseInt(word);
+		if (isNaN(value)) {
+			return accumulator;
+		} else {
+			return accumulator + value;
+		}
+	}, 0);
+
+	//Create inner HTML in movie details element
+	detailsElement.innerHTML = `
         <article class="media">
             <figure class="media-left">
                 <p class="image">
@@ -68,23 +99,24 @@ const renderMovieDetails = (movieDetails, parentElement) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-light">
+
+        <article data-value=${awards} class="notification is-light">
             <p class="title">${movieDetails.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-light">
+        <article data-value=${boxOffice} class="notification is-light">
             <p class="title">${movieDetails.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-light">
+        <article data-value=${metascore} class="notification is-light">
             <p class="title">${movieDetails.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-light">
+        <article data-value=${imdbRating} class="notification is-light">
             <p class="title">${movieDetails.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-light">
+        <article data-value=${imdbVotes} class="notification is-light">
             <p class="title">${movieDetails.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
